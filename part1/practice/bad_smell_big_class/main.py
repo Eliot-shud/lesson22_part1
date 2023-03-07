b78d951edcd33de21e2f4fa4104d0efbd9816862
Eliot-shud <elyarkamol1630@gmail.com>
2023-03-07 15:39:55 +0700

    finished

--- part1/practice/bad_smell_big_class/main.py
+++ part1/practice/bad_smell_big_class/main.py
@@ -10,14 +10,15 @@
 # Ловушка не может ничего кроме как атаковать того, кто на нее наступит
 # Для решения этой задачи не используйте наследование
 
-class Obj:
     ##
     # тут представлено поведение четырех различных игровых объектов:
     # - воина
     # - лекаря
     # - дерева
-    # - ловушки
+    # - ловушka
 
+
+class Soldier:
     def attack(self):
         pass
 
@@ -27,6 +28,9 @@ class Obj:
     def move(self):
         pass
 
+
+
+class Healer:
     def healer_defense(self):
         pass
 
@@ -36,16 +40,21 @@ class Obj:
     def heal(self):
         pass
 
+
+class Tree:
     def tree_defense(self):
         pass
 
     def on_fire(self):
         pass
 
-    def trap_attack(self):
-        print("It's a trap!")
+
+class Trap:
+    def attack(self):
+        print('Nooooooo')
+
 
 if __name__ == '__main__':
-    unit = Warrior()
+
     healer = Healer()
     trap = Trap()
